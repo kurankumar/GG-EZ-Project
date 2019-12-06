984eaad2ddb5f990e8d415e28f7ebfdc0c46008b
Kuran <kurankumar@gmail.com>
2019-12-06 10:38:44 -0500

    updated some routes and user controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,28 @@
 class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users
+    render json: @users.to_json(:include => :scores)
   end
 
   def show
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
     render json: @user
   end
 
   def create
-    @user = User.find_or_create_by(username: user_params[:username])
+    @user = User.create(username: user_params[:username])
     render json: @user
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: { user: @user, message: "User DESTROYYYYYED!" }, status: :ok
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
+      resources :users
       resources :games, only: [:index, :show]
       resources :genres, only: [:index, :show]
       resources :scores, only: [:index, :show]
